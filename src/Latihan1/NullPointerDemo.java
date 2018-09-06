982aea76c7b80bfd2e89f7c8796f5ded251a99d4
ASUS <ASUS@DESKTOP-OM3DURN>
2018-09-06 07:54:12 +0700

    commit keseluruhan jobsheet3

---
+++ src/Latihan1/NullPointerDemo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class NullPointerDemo {
+   public static void main(String[] args) {
+       String name ="isi nama masing";
+       System.out.print("Length of the string"+ name.length());
+    }
+
+    
+}
---
+++ src/Latihan4/InputMismatchException.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class InputMismatchException {
+    public static void main(String[] args) {
+         //int a =100;
+        System.out.println("Masukan nilai a");
+        Scanner console = new Scanner(System.in);
+        int a =console.nextInt();
+        System.out.println("Masukan nilai b");
+        int c = console.nextInt();
+       // int res=10/(b-c);
+      //  System.out.println("Hasilnya adalah  "+res);
+    }
+  
+}
